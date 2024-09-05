460ae6e87eb226c4c055bd7935f20eb3abc4028b
황세진 <stork0827@gmail.com>
2024-09-05 12:46:08 +0900

    등산로 경로 지도에 띄우는 중

--- lib/service/trail_service.dart
+++ lib/service/trail_service.dart
@@ -25,4 +25,30 @@ class TrailService{
       throw Exception('서버 요청 실패');
     }
   }
+
+  // 스팟 가져오기
+  Future<List<dynamic>> selectSpotsByTrailId(int trailIdx) async{
+    try {
+      String url = "http://192.168.219.200:8000/trail/selectSpotsByTrailId";
+      Response res = await dio.get(url, queryParameters: {
+        'trailIdx': trailIdx,
+      });
+
+      print('Request URL: ${res.realUri}');
+      print('Status Code: ${res.statusCode}');
+      print('Response Data: ${res.data}');
+
+      return res.data['spots']; // 성공 시 데이터 리스트 반환
+
+      // if (res.data['success']) {
+      //   return res.data['spots']; // 성공 시 데이터 리스트 반환
+      // } else {
+      //   throw Exception('검색 실패');
+      // }
+    } catch (e) {
+      print('Error occurred: $e');
+      throw Exception('서버 요청 실패');
+    }
+
+  }
 }
