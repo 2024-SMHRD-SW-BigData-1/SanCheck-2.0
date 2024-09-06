c7b9f2d676577ef389c8abc4dd1653f14049d4ff
정지인 <nono8465@naver.com>
2024-09-06 19:24:02 +0900

    등산하기 카메라 순서 수정

--- lib/screen/hike.dart
+++ lib/screen/hike.dart
@@ -7,7 +7,6 @@ import 'package:location/location.dart';
 import 'package:sancheck/globals.dart';
 import 'package:sancheck/screen/hike_record.dart';
 import 'package:shared_preferences/shared_preferences.dart';  // SharedPreferences 추가
-import 'package:shared_preferences/shared_preferences.dart'; // SharedPreferences 추가
 import 'package:image_picker/image_picker.dart'; // 이미지 픽커 추가
 import 'package:sancheck/screen/weather.dart';
 import 'package:sancheck/screen/hike_record.dart'; // HikeRecordModal 정의 파일
@@ -86,67 +85,61 @@ class _HikeState extends State<Hike> {
     _initialize();
   }
 
+  Future<void> _initialize() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    Location location = Location();
+    final LocationData currentLocation = await location.getLocation();
 
+    double nx = (currentLocation.latitude!);
+    double ny = (currentLocation.longitude!);
 
-Future<void> _initialize() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  Location location = Location();
-  final LocationData currentLocation = await location.getLocation();
+    _cameraPosition = NCameraPosition(
+      target: NLatLng(nx, ny),
+      zoom: 10,
+      bearing: 0,
+      tilt: 0,
+    );
+    // 네이버 앱 인증
+    await NaverMapSdk.instance.initialize(
+      clientId: '119m2j9zpj',
+      onAuthFailed: (ex) {
+        print("********* 네이버맵 인증오류 : $ex *********");
+      },
+    );
 
-  double nx = (currentLocation.latitude!);
-  double ny = (currentLocation.longitude!);
-
-
-
-  _cameraPosition = NCameraPosition(
-  target: NLatLng(nx, ny),
-  zoom: 10,
-  bearing: 0,
-  tilt: 0,
-  );
-  // 네이버 앱 인증
-  await NaverMapSdk.instance.initialize(
-    clientId: '119m2j9zpj',
-    onAuthFailed: (ex) {
-    print("********* 네이버맵 인증오류 : $ex *********");
-    },
-  );
-
-  // 위치 서비스 확인 및 요청
-  bool _serviceEnabled = await location.serviceEnabled();
-  if (!_serviceEnabled) {
-    _serviceEnabled = await location.requestService();
+    // 위치 서비스 확인 및 요청
+    bool _serviceEnabled = await location.serviceEnabled();
     if (!_serviceEnabled) {
-      return;
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
     }
+
+    // 위치 권한 확인 및 요청
+    PermissionStatus _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+    await _loadTimerValue(); // 앱 시작 시 저장된 타이머 값 불러오기
+
+    // 경로 정보 set
+    setState(() {
+      _coords = selectedTrail!['trail_path']!.map<NLatLng>((point) {
+        return NLatLng(point['x'], point['y']);
+      }).toList();
+    });
   }
 
-  // 위치 권한 확인 및 요청
-  PermissionStatus _permissionGranted = await location.hasPermission();
-  if (_permissionGranted == PermissionStatus.denied) {
-    _permissionGranted = await location.requestPermission();
-    if (_permissionGranted != PermissionStatus.granted) {
-      return;
-    }
-  }
-  await _loadTimerValue();  // 앱 시작 시 저장된 타이머 값 불러오기
-
-
-  // 경로 정보 set
-  setState(() {
-    _coords = selectedTrail!['trail_path']!.map<NLatLng>((point) {
-      return NLatLng(point['x'], point['y']);
-    }).toList();
-  });
-
-}
-
-// spot을 지도에 추가하는 함수
+  // spot을 지도에 추가하는 함수
   Future<void> addSpotsToMap(controller) async {
-    if (selectedSpots!.isEmpty || selectedSpots==null){
+    if (selectedSpots!.isEmpty || selectedSpots == null) {
       return;
     }
-    NOverlayImage image =  await NOverlayImage.fromWidget(widget: Icon(Icons.add_alert), size: Size(5.0, 5.0), context: context);
+    NOverlayImage image = await NOverlayImage.fromWidget(widget: Icon(Icons.add_alert), size: Size(5.0, 5.0), context: context);
     // 맵 컨트롤러가 준비된 후 마커를 추가하도록 수정
     WidgetsBinding.instance.addPostFrameCallback((_) {
       selectedSpots!.forEach((spot) {
@@ -154,7 +147,7 @@ Future<void> _initialize() async{
         final marker = NMarker(
           id: spot['spot_idx'].toString(), // 스팟의 고유 ID
           icon: image,
-          position: NLatLng(double.parse(spot['spot_latitude']),double.parse(spot['spot_longitude'])), // 마커의 위치 설정
+          position: NLatLng(double.parse(spot['spot_latitude']), double.parse(spot['spot_longitude'])), // 마커의 위치 설정
           size: Size(20, 25), // 마커의 크기 설정
           caption: NOverlayCaption(text: spot['spot_name'], textSize: 12.0),
           isHideCollidedSymbols: true,
@@ -162,15 +155,12 @@ Future<void> _initialize() async{
 
         // 마커를 지도에 추가
         controller.addOverlay(marker);
-        marker.setOnTapListener(
-                (NMarker marker)=>{
-              print("마커 클릭됨"),
-            }
-        );
+        marker.setOnTapListener((NMarker marker) => {
+          print("마커 클릭됨"),
+        });
       });
     });
   }
-
 
   Future<void> _initializeNaverMapSdk() async {
     await NaverMapSdk.instance.initialize(clientId: '119m2j9zpj');
@@ -217,6 +207,7 @@ Future<void> _initialize() async{
     );
   }
 
+  // 이미지 촬영 함수 수정
   Future<void> _captureImage() async {
     final picker = ImagePicker();
     final pickedFile = await picker.pickImage(source: ImageSource.camera);
@@ -228,39 +219,13 @@ Future<void> _initialize() async{
       // 조건 체크 후 메달 모달 띄우기
       bool conditionMet = _capturedImage != null; // 실제 조건 체크 로직으로 교체
       if (conditionMet) {
-        _showMedalModal();
+        _showMedalModal(); // 메달 모달을 띄우고
+      } else {
+        _showHikeRecodeModal(); // 조건을 만족하지 않으면 등산 기록 모달로 이동
       }
+    } else {
+      _showHikeRecodeModal(); // 사진을 찍지 않았을 경우에도 등산 기록 모달로 이동
     }
-  }
-
-  void _showHikeRecodeModal() {
-    showDialog(
-      context: context,
-      barrierColor: Colors.black.withOpacity(0.5), // 모달 밖 배경 어둡게 설정
-      builder: (BuildContext context) {
-        return Dialog(
-          backgroundColor: Colors.white, // 모달의 배경을 하얗게 설정
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20.0),
-          ),
-          child: HikeRecordModal(), // 기존의 HikeRecordModal 위젯
-        );
-      },
-    ).then((_) {
-      // HikeRecordModal이 닫힌 후 카메라 촬영 기능 실행
-      _captureImage();
-    });
-  }
-
-  void _showMedalModal() {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return MedalModal(
-          medalImageUrl: 'https://example.com/medal.png', // 실제 메달 이미지 URL로 교체
-        );
-      },
-    );
   }
 
   void _toggleTracking() {
@@ -292,65 +257,64 @@ Future<void> _initialize() async{
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          backgroundColor: Colors.white, // 모달창 배경색을 하얀색으로 설정
+          backgroundColor: Colors.white,
           shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20.0), // 둥근 모서리 설정
+            borderRadius: BorderRadius.circular(20.0),
           ),
           title: Center(
             child: Text(
               '등산을 그만할까요?',
               style: TextStyle(
                 fontWeight: FontWeight.bold,
-                fontSize: 20, // 글씨 크기 2p 키움
+                fontSize: 20,
               ),
             ),
           ),
           content: Text(
             '진행 중인 등산을 중단하고\n기록을 저장하시겠습니까?',
             textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 18), // 글씨 크기 2p 키움
+            style: TextStyle(fontSize: 18),
           ),
           actions: <Widget>[
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 SizedBox(
-                  width: 100, // 버튼의 동일한 너비 설정
+                  width: 100,
                   child: ElevatedButton(
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.green,
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(12),
                       ),
-                      padding: EdgeInsets.symmetric(vertical: 12), // 버튼의 높이 조정
+                      padding: EdgeInsets.symmetric(vertical: 12),
                     ),
                     onPressed: () {
-                      Navigator.of(context).pop(); // 모달창 닫기
+                      Navigator.of(context).pop();
                     },
                     child: Text(
                       '아니오',
-                      style: TextStyle(color: Colors.white, fontSize: 16), // 글씨 크기 2p 키움
+                      style: TextStyle(color: Colors.white, fontSize: 16),
                     ),
                   ),
                 ),
                 SizedBox(
-                  width: 100, // 버튼의 동일한 너비 설정
+                  width: 100,
                   child: ElevatedButton(
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.red,
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(12),
                       ),
-                      padding: EdgeInsets.symmetric(vertical: 12), // 버튼의 높이 조정
+                      padding: EdgeInsets.symmetric(vertical: 12),
                     ),
                     onPressed: () {
-                      _resetTimer();
-                      Navigator.of(context).pop(); // 모달창 닫기 후 상태 초기화
-                      _showHikeRecodeModal(); // 등산 기록 모달 호출
+                      Navigator.of(context).pop();
+                      _showPhotoOptionModal(); // 사진 촬영 여부 묻는 모달 호출
                     },
                     child: Text(
                       '예',
-                      style: TextStyle(color: Colors.white, fontSize: 16), // 글씨 크기 2p 키움
+                      style: TextStyle(color: Colors.white, fontSize: 16),
                     ),
                   ),
                 ),
@@ -361,6 +325,120 @@ Future<void> _initialize() async{
       },
     );
   }
+
+  // 사진 촬영 여부 묻는 모달
+  void _showPhotoOptionModal() {
+    showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          title: Center(
+            child: Text(
+              '사진 촬영을 하시겠습니까?',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          ),
+          actions: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SizedBox(
+                  width: 100,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      padding: EdgeInsets.symmetric(vertical: 12),
+                    ),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                      _showHikeRecodeModal(); // 등산 기록 모달 호출
+                    },
+                    child: Text(
+                      '아니오',
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 100,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      padding: EdgeInsets.symmetric(vertical: 12),
+                    ),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                      _captureImage(); // 카메라 촬영 호출
+                    },
+                    child: Text(
+                      '예',
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  // 메달 모달을 닫으면 등산 기록 모달을 호출
+  void _showMedalModal() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return MedalModal(
+          medalImageUrl: 'https://example.com/medal.png',
+        );
+      },
+    ).then((_) {
+      _showHikeRecodeModal(); // 메달 모달이 닫힌 후 등산 기록 모달 호출
+    });
+  }
+
+  // 등산 기록 모달
+  void _showHikeRecodeModal() {
+    showDialog(
+      context: context,
+      barrierColor: Colors.black.withOpacity(0.5),
+      builder: (BuildContext context) {
+        return Dialog(
+          backgroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          child: HikeRecordModal(),
+        );
+      },
+    ).then((_) {
+      // 등산 기록 모달이 닫힌 후 등산하기 페이지로 돌아옴
+      if (Navigator.canPop(context)) {
+        Navigator.pop(context); // 등산하기 페이지로 이동
+      } else {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(
+            builder: (context) => Hike(), // 등산하기 페이지로 돌아가기
+          ),
+        );
+      }
+    });
+  }
+
 
   void _resetTimer() {
     _pauseTimer();
@@ -409,17 +487,17 @@ Future<void> _initialize() async{
               title: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  if(selectedTrail!=null && !_isTracking)
-                        Text(
-                          '선택된 등산로: ${selectedTrail!['trail_name']} ',
-                          style: TextStyle(fontSize: 16),
-                        ),
-                  if(selectedTrail==null && !_isTracking)
+                  if (selectedTrail != null && !_isTracking)
+                    Text(
+                      '선택된 등산로: ${selectedTrail!['trail_name']} ',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  if (selectedTrail == null && !_isTracking)
                     Text(
                       '선택된 등산로: 없음 ',
                       style: TextStyle(fontSize: 16),
                     ),
-                  if (selectedTrail==null && _isTracking)
+                  if (selectedTrail == null && _isTracking)
                     Row(
                       children: [
                         Text(
@@ -437,7 +515,7 @@ Future<void> _initialize() async{
                         ),
                       ],
                     ),
-                  if(selectedTrail!=null && _isTracking)
+                  if (selectedTrail != null && _isTracking)
                     Column(
                       children: [
                         Text(
@@ -475,7 +553,7 @@ Future<void> _initialize() async{
                       selectedTrail = null;
                       selectedMountain = null;
                       selectedSpots = null;
-                      _coords= [];
+                      _coords = [];
                     });
                   },
                 ),
@@ -504,7 +582,7 @@ Future<void> _initialize() async{
                     controller.setLocationTrackingMode(mode);
 
                     // 경로가 있는 경우 NPathOverlay를 추가
-                    if (_coords.isNotEmpty && selectedSpots!=null) {
+                    if (_coords.isNotEmpty && selectedSpots != null) {
                       var route = NPathOverlay(
                         id: '$selectedTrail["trail_idx"]',
                         coords: _coords,
@@ -516,10 +594,13 @@ Future<void> _initialize() async{
                       addSpotsToMap(controller);
 
                       controller.updateCamera(NCameraUpdate.withParams(
-                        target: NLatLng(double.parse(selectedMountain!['mount_latitude']), double.parse(selectedMountain!['mount_longitude'])),
+                        target: NLatLng(
+                          double.parse(selectedMountain!['mount_latitude']),
+                          double.parse(selectedMountain!['mount_longitude']),
+                        ),
                         zoom: 11,
                         bearing: 0,
-                        tilt: 0
+                        tilt: 0,
                       ));
                     }
                   },
